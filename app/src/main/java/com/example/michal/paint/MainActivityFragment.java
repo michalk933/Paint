e8acc153a9d69d94b7341bb1abfdca7421c04d24
michalk933 <6pak.tv@gmail.com>
2016-11-02 10:53:51 +0100

    menu visible, but not visible icon set color

--- app/src/main/java/com/example/michal/paint/MainActivityFragment.java
+++ app/src/main/java/com/example/michal/paint/MainActivityFragment.java
@@ -5,28 +5,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Collection;
 import java.util.zip.Inflater;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private FildDrawing fildDrawing;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        super.onCreateView(inflater,container,savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
         setHasOptionsMenu(true);// set visible menu
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        fildDrawing = (FildDrawing) view.findViewById(R.id.fildDrawingFragment);
+        return view;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_paint,menu);
         super.onCreateOptionsMenu(menu, inflater);
-        Inflater.inflate(R.menu.menu_paint,menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.color_menu:
+                ColorDialog colorDialog = new ColorDialog();
+                colorDialog.show(getFragmentManager(), "color");
+                return true;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
